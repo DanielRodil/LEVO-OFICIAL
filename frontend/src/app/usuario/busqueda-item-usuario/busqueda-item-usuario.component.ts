5d10077686b6330a837f694352ba20233bc3c9de
sdrfdim <sdrfdim@gmail.com>
2022-11-22 10:26:35 +0100

    avisomes en consulta usuario

--- frontend/src/app/usuario/busqueda-item-usuario/busqueda-item-usuario.component.ts
+++ frontend/src/app/usuario/busqueda-item-usuario/busqueda-item-usuario.component.ts
@@ -1,7 +1,10 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Avisokm } from 'src/app/administrador/models/avisokm';
 import { AvisokmImpl } from 'src/app/administrador/models/avisokm-impl';
+import { Avisomes } from 'src/app/administrador/models/avisomes';
+import { AvisomesImpl } from 'src/app/administrador/models/avisomes-impl';
 import { AvisokmService } from 'src/app/administrador/service/avisokm.service';
+import { AvisomesService } from 'src/app/administrador/service/avisomes.service';
 import { Vehiculo } from 'src/app/vehiculo/models/vehiculo';
 import { VehiculoImpl } from 'src/app/vehiculo/models/vehiculo-impl';
 
@@ -18,13 +21,18 @@ export class BusquedaItemUsuarioComponent implements OnInit {
   @Output() vehiculoConsultar = new EventEmitter<VehiculoImpl>();
 
   avisokm: Avisokm = new AvisokmImpl;
+  avisomes: Avisomes = new AvisomesImpl;
 
-  constructor(private avisokmService: AvisokmService) { }
+  constructor(private avisokmService: AvisokmService,
+              private avisomesService: AvisomesService) { }
 
   ngOnInit(): void {
     this.avisokmService.getAvisoKmBusqueda(this.vehiculo.avisokms).subscribe(response => {
       this.avisokm = this.avisokmService.extraerAvisoKm(response);
       console.log(response);
+    })
+    this.avisomesService.getAvisoMesBusqueda(this.vehiculo.avisomes).subscribe(response => {
+      this.avisomes = this.avisomesService.extraerAvisoMes(response);
     })
   }
 
